b0fad69d5de007a71fe763e03bdd3c1efed1ad40
MarkR91 <84676012+MarkR91@users.noreply.github.com>
2022-08-17 00:09:45 -0400

    Add files via upload
    hovering added

--- src/vertical_climb.py
+++ src/vertical_climb.py
@@ -5,7 +5,8 @@ from std_msgs.msg import String
 import math
 import geometry_msgs.msg
 
-def cmd_command():
+
+def takeoff_command():
     
     cmd = geometry_msgs.msg.Twist()
  
@@ -15,21 +16,51 @@ def cmd_command():
     cmd.linear.x =0
     cmd.linear.y =0
     cmd.linear.z =4
+    
 
     cmd.angular.x =0
     cmd.angular.y =0
     cmd.angular.z =0
    
     return cmd
+
+def hover_command():
+    
+    cmd = geometry_msgs.msg.Twist()
+ 
+
+    cmd.linear.x =0
+    cmd.linear.y =0
+    cmd.linear.z =0
+    
+
+    cmd.angular.x =0
+    cmd.angular.y =0
+    cmd.angular.z =0
+   
+    return cmd
+
+
+
     
 def open_loop():
     rospy.init_node('open_loop', anonymous=True)
-    quad_vel = rospy.Publisher('cmd_vel', geometry_msgs.msg.Twist,queue_size=1)
+    quad_vel = rospy.Publisher('cmd_vel',geometry_msgs.msg.Twist ,queue_size=1)
     rate = rospy.Rate(10) # 10hz
+    
+    n=0
+    print("climbing")
     while not rospy.is_shutdown():
-
-        quad_vel.publish(cmd_command())
+        quad_vel.publish(takeoff_command())
+        if n>=30:
+           quad_vel.publish(hover_command())
+           print("hovering")
+        n=n+1
         rate.sleep()
+     
+    
+
+ 
 
 if __name__ == '__main__':
 	try:
